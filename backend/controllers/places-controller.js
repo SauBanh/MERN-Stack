044e6a8bd01e90b6c21b58ae20839262a97fc22f
SauBanh <saubanh05062001@gmail.com>
2023-05-06 12:39:00 +0700

    handle get all place by userid

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -28,13 +28,13 @@ const getPlaceById = (req, res, next) => {
     res.json({ place }); //{ place } => { place: place }
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
     const userId = req.params.uid;
 
-    const place = DUMMY_PLACES.find((p) => {
+    const places = DUMMY_PLACES.filter((p) => {
         return p.creator === userId;
     });
-    if (!place) {
+    if (!places || places.length === 0) {
         return next(
             new HttpError(
                 "Could not find a place for the provided user id.",
@@ -42,7 +42,7 @@ const getPlaceByUserId = (req, res, next) => {
             )
         );
     }
-    res.json({ place });
+    res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -77,7 +77,7 @@ const deletePlaceById = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlaceById = updatePlaceById;
 exports.deletePlaceById = deletePlaceById;
